21c22c9cefa633416f3f6dbc0531f1657b88c01d
andischw12 <andischw12@gmail.com>
2022-02-03 14:12:10 +0200

    Wow6

--- Assets/PowerUpUIButton.cs
+++ Assets/PowerUpUIButton.cs
@@ -6,22 +6,35 @@ public abstract class PowerUpUIButton : MonoBehaviour
 {
     [SerializeField] Button MyButton;
     int PowerUpVal;
+    Image[] arr;
     public void Start()
     {
         if (GetComponentInChildren<Button>())
         {
             MyButton = GetComponentInChildren<Button>();
             MyButton.onClick.AddListener(() => Use(PowerUpVal));
+            arr = GetComponentsInChildren<Image>();
         }
     }
     public void Disable()
     {
         MyButton.interactable = false;
+        
+        arr[0].color = new Color32(123, 123, 123, 255);
+        arr[1].gameObject.SetActive(false);
+        arr[2].gameObject.SetActive(false);
+        arr[3].gameObject.SetActive(true);
     }
     public void Enable(int val)
     {
         PowerUpVal = val;
         MyButton.interactable = true;
+       
+        arr[0].color = new Color32(0, 96, 149, 222);
+        arr[1].gameObject.SetActive(true);
+        arr[2].gameObject.SetActive(true);
+         arr[3].gameObject.SetActive(false);
+        
     }
     public virtual void Use(float PowerUpVal)
     {
